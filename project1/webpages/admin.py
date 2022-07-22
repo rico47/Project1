25c439c28eb90030966eea316d915e637fa32030
ravw <admin@e-host.org>
2022-07-22 11:15:48 +0200

    add new

--- project1/webpages/admin.py
+++ project1/webpages/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Webpage, Stats
 
-# Register your models here.
+
+@admin.register(Webpage)
+class WebpageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description']
+
+
+@admin.register(Stats)
+class StatsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'idx_js', 'idx_html', 'idx_bool']
+
--- project1/webpages/models.py
+++ project1/webpages/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Webpage(models.Model):
     title = models.CharField(max_length=255)
-    url = models.URLField(max_length=200)
-    description = models.TextField()
+    url = models.URLField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
 
 
 class Stats(models.Model):
@@ -12,7 +12,7 @@ class Stats(models.Model):
     title = models.CharField(max_length=255)
     idx_js = models.IntegerField()
     idx_html = models.IntegerField()
-    idx_image = models.ImageField()
+    idx_image = models.ImageField(null=True, blank=True)
     idx_bool = models.BooleanField()
 
 
