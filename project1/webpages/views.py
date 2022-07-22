3f2106b346129d15661863ed692b42fe1843263a
ravw <admin@e-host.org>
2022-07-22 12:26:56 +0200

    add new

--- project1/webpages/views.py
+++ project1/webpages/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+from .models import Webpage, Stats
+
+# description, id, stats, title, url
+
+
+def index(request):
+    latest_question_list = Webpage.objects.order_by('title')[:5]
+    template = loader.get_template('webpages/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
