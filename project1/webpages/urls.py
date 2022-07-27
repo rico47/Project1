a8055a145cfd8b2dfdafad735eb9d92a2fb0e389
ravw <admin@e-host.org>
2022-07-27 08:27:40 +0200

    changes

--- project1/webpages/urls.py
+++ project1/webpages/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 
 from . import views
 
 app_name = 'webpages'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('', views.navbar, name='navbar')
 ]
 
--- project1/webpages/views.py
+++ project1/webpages/views.py
@@ -6,10 +6,15 @@ from .models import Webpage, Stats
 # description, id, stats, title, url
 
 
-def index(request):
+def navbar(request):
     latest_question_list = Webpage.objects.order_by('title')[:5]
-    template = loader.get_template('webpages/index.html')
+    template = loader.get_template('webpages/navbar.html')
     context = {
         'latest_question_list': latest_question_list,
     }
     return HttpResponse(template.render(context, request))
+
+
+def index(request):
+    pass
+
